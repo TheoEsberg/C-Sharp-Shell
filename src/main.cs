b5cf4cdd6e9167ffe88aeab74428b131eea9ab86
theoesberg <esbergtheo@gmail.com>
2025-06-09 11:45:44 +0200

    adding type command

--- src/main.cs
+++ src/main.cs
@@ -1,20 +1,47 @@
+using System.Diagnostics.Tracing;
 using System.Net;
 using System.Net.Sockets;
 
+
+List<string> Types = new List<string>()
+{
+    "exit", "echo", "type"
+};
+
+// REPL (Read-Eval-Print Loop) for a simple command line interface
 while (true)
 {
     Console.Write("$ ");
-    var command = Console.ReadLine();
+    string? command = Console.ReadLine();
 
-    if (command!.Split(" ")[0] == "exit")
+    if (string.IsNullOrEmpty(command))
+    {
+        continue;
+    }
+
+    // If user input is exit, we exit the program with a status code
+    else if (command.Split(" ")[0].ToLower() == "exit")
     {
         Environment.Exit(int.Parse(command.Split(" ")[1]));
     }
 
-    if (command!.Split(" ")[0] == "echo")
+    // If user input is echo, we print the rest of the comman
+    else if (command.ToLower().StartsWith("echo") == true)
     {
-        var response = command.Replace("echo", "").Trim();
-        Console.WriteLine(response);
+        Console.WriteLine(command[5..]);
+    }
+
+    else if(command.ToLower().StartsWith("type") == true)
+    {
+        string commandType = command.Split(" ")[1];
+        if (Types.Contains(commandType.ToLower()))
+        {
+            Console.WriteLine($"{commandType} is a shell builtin");
+        }
+        else
+        {
+            Console.WriteLine($"{commandType}: not found");
+        }
     }
 
     else {

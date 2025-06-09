7ce90d013da88cba6ffca50f9df3306d03ff98e1
theoesberg <esbergtheo@gmail.com>
2025-06-09 10:35:46 +0200

    Added the $ before each row

--- src/main.cs
+++ src/main.cs
@@ -1,11 +1,11 @@
 using System.Net;
 using System.Net.Sockets;
 
-Console.Write("$ ");
 
 // Wait for user input
 while (true)
 {
+    Console.Write("$ ");
     var command = Console.ReadLine();
     Console.WriteLine($"{command}: command not found");
 }
